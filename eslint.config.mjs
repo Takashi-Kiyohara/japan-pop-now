b3e2ea7565d1eb13eb754a106e6d2eb4af3e8b44
Takapon <takashi03157@gmail.com>
2026-04-11 12:37:46 +0900

    chore: exclude image-pipeline and data/face-models from eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,9 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    // Exclude image-pipeline and face-models
+    "image-pipeline/**",
+    "data/face-models/**",
   ]),
 ]);
 
